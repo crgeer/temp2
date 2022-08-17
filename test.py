3ff0a082ee5904b3ea125b96515993e118500c74
crgeer <connor.geer@protonmail.com>
2022-08-17 13:17:55 -0700

    adding a file to test repeating the classify script on all contents of Common Voice

--- test.py
+++ test.py
@@ -4,5 +4,13 @@ args = "-alh"
 
 subprocess.run(["ls", args])
 
-for item in os.listdir("."):
-	print(item)
+arg1 = 'classify.py'
+arg2 = '--in'
+arg3 = '../CommonVoice/sample-000000.wav'
+arg4 = '--restore_path'
+arg5 = 'deepspeech-0.4.1-checkpoint/model.v0.4.1'
+
+#subprocess.run(['python3', arg1, arg2, arg3, arg4, arg5])
+
+for item in os.listdir("/content/CommonVoice"):
+	subprocess.run(["python3", arg1, arg2, '/content/CommonVoice/' + item, arg4, arg5])
